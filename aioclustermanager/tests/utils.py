18d175fb3da3bc923a4164ca843fd5c91620e92a
Ramon Navarro Bosch <ramon.nb@gmail.com>
2019-09-29 19:25:23 +0200

    Fixing CA

--- aioclustermanager/tests/utils.py
+++ aioclustermanager/tests/utils.py
@@ -77,7 +77,8 @@ def get_k8s_config():
         ca_info = cluster_info["cluster"].get("certificate-authority-data")
         if ca_info is None:
             ca_info = cluster_info["cluster"].get("certificate-authority")
-
-        config_k8s["ca"] = ca_info
+            config_k8s["ca_file"] = ca_info
+        else:
+            config_k8s["ca"] = ca_info
 
     return config_k8s
